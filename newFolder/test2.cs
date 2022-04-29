2d5f43636e1182f945eba4165cc3a17e25aaf094
MichiMichiRu <michimichiru75@gmail.com>
2022-04-30 00:45:38 +0300

    Modifed test logic

---
+++ newFolder/test2.cs
@@ -0,0 +1,16 @@
+public class main {
+
+    int y = 0;
+
+    
+    public int SomeFunc(int x)
+    {
+        x = x * 2;
+        return x;
+    }
+
+    y = Console.ReadLine();
+
+    WriteLine(SomeFunc(x));
+
+}
